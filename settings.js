58186c7f4a439e86e91ae0d716947ca6c5358e28
Danielrajput2 <161234727+Danielrajput2@users.noreply.github.com>
2025-02-21 21:45:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'KING B2K',
-  author: '',
-  botName: "KING B2K",
-  botOwner: 'KING B2K', // Your name
-  ownerNumber: '918512849649', //Your number
+  packname: 'KING B2K ✓',
+  author: '❤️NAVU ✓',
+  botName: "KING B2K ✓",
+  botOwner: 'KING B2K ✓', // Your name
+  ownerNumber: '919654934474', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
